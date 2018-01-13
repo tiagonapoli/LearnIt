051083ca10dbe82be5e67a06ba6b14eff1cfed01
Gabriel <gacamargo1.000@gmail.com>
2018-01-13 15:35:05 -0200

    Create 'languages' relation on tables.sql

--- tables.sql
+++ tables.sql
@@ -3,21 +3,28 @@ CREATE TABLE users(
 	messages_per_day integer
 );
 
+CREATE TABLE languages(
+	user_id int,
+	language_name varchar(50),
+	primary key (user_id, language_name),
+	foreign key (user_id) references users(id) ON DELETE CASCADE
+);
+
 CREATE TABLE words(
 	user_id int,
-	idiom varchar(50),
+	language varchar(50),
 	foreign_word varchar(50),
 	english_word varchar(50),
-	primary key (user_id, idiom, foreign_word),
-	foreign key (user_id) references users(id) ON DELETE CASCADE
+	primary key (user_id, language, foreign_word),
+	foreign key (user_id, language) references languages(user_id, language_name) ON DELETE CASCADE
 );
 
 CREATE TABLE images(
 	user_id int,
-	idiom varchar(50),
+	language varchar(50),
 	foreign_word varchar(50),
-	id serial,
-	img_path varchar(50),
-	primary key (user_id, idiom, foreign_word, id),
-	foreign key (user_id, idiom, foreign_word) references words(user_id, idiom, foreign_word) ON DELETE CASCADE
+	image_id serial,
+	image_path varchar(50),
+	primary key (user_id, language, foreign_word, image_id),
+	foreign key (user_id, language, foreign_word) references words(user_id, language, foreign_word) ON DELETE CASCADE
 );
