23336125ebcd4e1060c60249994bce04629b8714
tiagonapoli <napoli.tiago@hotmail.com>
2018-01-14 21:05:16 -0200

    SM2 variable and constructor changes

--- SM2.py
+++ SM2.py
@@ -5,21 +5,13 @@ class FlashCardSM:
 	n = None
 	ef = None
 	interval = None
-	last_date = None
+	next_date = None
 
-	def __init__(self,_n=0, _ef=2.5,_interval=0, _last_date = datetime.datetime.now()):
+	def __init__(self,_n=1, _ef=2.5,_interval=1, _next_date = datetime.datetime.now() + datetime.timedelta(days=1)):
 		self.n = _n
 		self.ef = _ef
 		self.interval = _interval
-		self.last_date = _last_date
-
-
-	def set_variables(self,_n, _ef,_interval, _last_date = datetime.datetime.now()):
-		self.n = _n
-		self.ef = _ef
-		self.interval = _interval
-		self.last_date = _last_date
-
+		self.next_date = _next_date
 
 	def go_next_state(self,q):
 		self.n += 1
@@ -36,16 +28,17 @@ class FlashCardSM:
 			self.ef = 1.3
 
 	def get_next_date(self, quality_answer):
+		
 		self.go_next_state(quality_answer)
-		end_date = self.last_date + datetime.timedelta(days=int(self.interval))
+		end_date = self.next_date + datetime.timedelta(days=int(self.interval))
 
-		self.last_date = end_date
+		self.next_date = end_date
 		return end_date
 	
 	def get_date(self):
-		return last_date
+		return next_date
 
 	def get_data(self):
-		return self.n,self.ef,self.interval,self.last_date
+		return self.n,self.ef,self.interval,self.next_date
 
 
