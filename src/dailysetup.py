f40a796de25433800a22011f77cc1f36365131cc
tiagonapoli <napoli.tiago@hotmail.com>
2018-01-15 20:19:20 -0200

    Documentation lingbot, dailysetup, sender, systemtools

--- src/dailysetup.py
+++ src/dailysetup.py
@@ -1,21 +1,30 @@
 #! /usr/bin/python3
 import systemtools
 import datetime
-from RuntimeData import RuntimeData
-from FlashCard import Word
+from runtimedata import RuntimeData
+from flashcard import Card
+
+"""
+	Script that schedules all send jobs that have to be sent today.
+	
+	Global Constant:
+		ACTIVE_MINUTES: Minutes past 8 AM that the user is able to receive messages.
+
+"""
 
 ACTIVE_MINUTES = 60
 db = RuntimeData()
 today = datetime.datetime.now()
 
-for user in db.knowUsers:
-	cards = db.get_all_words_info(user)
+for user_id in db.get_known_users():
+	cards = db.get_all_words_info(user_id)
 	for card in cards:
 		cnt = 0
 		if card.get_date().date() <= today.date():
 			cnt += 1
-	time_acumulated = 0
+	time_acumulated = 5
 	for card in cards:
 		if card.get_date().date() <= today.date():
-			systemtools.set_new_at_job_card(time_accumulated + ACTIVE_MINUTES // cnt,user,card.wordID)
+			systemtools.set_new_at_job_card(time_accumulated + 
+					ACTIVE_MINUTES // cnt, user_id, card.wordID)
 			time_accumulated += ACTIVE_MINUTES // cnt
--- src/systemtools.py
+++ src/systemtools.py
@@ -1,18 +1,41 @@
 from subprocess import call
 
-def set_new_at_job_card(min_from_now,IDuser,IDcard,tried=0):
-	command = 'echo "./sender.py {} {} {}" | at now + {} min'.format(IDuser,IDcard,tried,min_from_now)
+def set_new_at_job_card(min_from_now,user_id,card_id,tried=0):
+	"""
+		Creates a new 'at' card job, i.e, schedules a card to be sent to an user.
+
+		Args:
+			min_from_now: the job will be schedulet at 'min_from_now' minutes
+			user_id: user id
+			card_id: card id
+			tried: number of unsuccessfull tries to send this card to this user
+	"""
+	
+	command = 'echo "./sender.py {} {} {}" | at now + {} min'.format(user_id,card_id,tried,min_from_now)
 	print(command)
 	call(command,shell=True)
 
-def schedule_daily_setup(days_from_now):
-	command = 'echo "./daily_setup.py" | at 8:00 tomorrow'.format(days_from_now)
+def schedule_daily_setup():
+	"""
+		Creates a new 'at' job to execute daily setup tomorrow, 8 AM.
+	"""
+
+	command = 'echo "./daily_setup.py" | at 8:00 tomorrow'
 	print(command)
 	call(command, shell=True)
 
 def schedule_setup_now():
-	command = 'echo "./daily_setup.py" | at now + 1 min'.format(days_from_now)
+	"""
+		Creates a new 'at' job to execute daily setup 1 minute from now.
+	"""
+	
+	command = 'echo "./daily_setup.py" | at now + 1 min'
 	print(command)
 	call(command, shell=True)
 
 
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
