a9a46e8e4d7ce3e286f6ea3040d04c25446c74af
tiagonapoli <napoli.tiago@hotmail.com>
2018-02-12 14:54:30 -0200

    upd

--- src/message_handlers/message_not_understood.py
+++ src/message_handlers/message_not_understood.py
@@ -12,8 +12,8 @@ def handle_message_not_understood(bot, rtd):
 		user_id = get_id(msg)
 		bot.send_message(user_id, "Oops, didn't understand your message")
 
-	@bot.callback_query_handler(func=lambda call: rtd.get_user(get_id(msg)).not_locked())
+	@bot.callback_query_handler(func=lambda call: True)
 	def callback_not_understood(call):
 		user_id = get_id(call.message)
 		print("Callback from user {} not understood or thrown away: {}".format(user_id, call.message.text))
-		bot.delete_message(chat_id=user_id, message_id=call.message.message_id)
+		#bot.delete_message(chat_id=user_id, message_id=call.message.message_id)
