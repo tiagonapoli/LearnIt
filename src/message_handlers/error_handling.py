21e44ff02a9783249aa2a552d2e02d1c7bc17bc6
tiagonapoli <napoli.tiago@hotmail.com>
2018-03-04 14:33:18 -0300

    bug fix

--- src/message_handlers/error_handling.py
+++ src/message_handlers/error_handling.py
@@ -17,7 +17,7 @@ def handle_user_dont_exist(bot, rtd, debug_mode):
 	@bot.callback_query_handler(func = lambda call:	rtd.check_user_existence(get_id(call.message)) == False)
 	def callback_user_existence(call):
 		user_id = get_id(call.message)
-		user = rtd.get_user(get_id(msg))
+		user = rtd.get_user(get_id(call.message))
 		error_msg = bot_language.translate("LearnIt is under development, so sometimes we have to do some experiments and reset some things, maybe because of this, your user isn't in our database. To fix this, please send a /start.", user)
 		bot.send_message(user_id, error_msg)
 
