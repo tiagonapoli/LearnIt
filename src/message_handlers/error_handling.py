592c4bfd9a08f7af52fc928651a3b0f2cb00a976
tiagonapoli <napoli.tiago@hotmail.com>
2018-03-04 14:43:39 -0300

    bug fix

--- src/message_handlers/error_handling.py
+++ src/message_handlers/error_handling.py
@@ -9,16 +9,14 @@ def handle_user_dont_exist(bot, rtd, debug_mode):
 	@bot.message_handler(func = lambda msg:	rtd.check_user_existence(get_id(msg)) == False, content_types=['text','photo','audio','voice'])
 	def user_existence(msg):
 		user_id = get_id(msg)
-		user = rtd.get_user(get_id(msg))
-		error_msg = bot_language.translate("LearnIt is under development, so sometimes we have to do some experiments and reset some things, maybe because of this, your user isn't in our database. To fix this, please send a /start.", user)
+		error_msg = ("LearnIt is under development, so sometimes we have to do some experiments and reset some things, maybe because of this, your user isn't in our database. To fix this, please send a /start.")
 		bot.send_message(user_id, error_msg)
 
 	#=====================USER DOESN'T EXIST - CALLBACK=====================
 	@bot.callback_query_handler(func = lambda call:	rtd.check_user_existence(get_id(call.message)) == False)
 	def callback_user_existence(call):
 		user_id = get_id(call.message)
-		user = rtd.get_user(get_id(call.message))
-		error_msg = bot_language.translate("LearnIt is under development, so sometimes we have to do some experiments and reset some things, maybe because of this, your user isn't in our database. To fix this, please send a /start.", user)
+		error_msg = ("LearnIt is under development, so sometimes we have to do some experiments and reset some things, maybe because of this, your user isn't in our database. To fix this, please send a /start.")
 		bot.send_message(user_id, error_msg)
 
 
